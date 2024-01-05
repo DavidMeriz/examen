c2238d7a28ae3986f82b7fd07b471e4d6ecbb378
DavidMeriz <davidecheverria1028@hotmail.com>
2024-01-04 19:49:36 -0500

    hoteles

--- Proyecto4/Views/reservas/reservas.php
+++ Proyecto4/Views/reservas/reservas.php
@@ -5,11 +5,11 @@
     <div class="col-lg-8 d-flex align-items-stretch">
         <div class="card w-100">
             <div class="card-body p-4">
-                <h5 class="card-title fw-semibold mb-4">Lista de Hoteles</h5>
+                <h5 class="card-title fw-semibold mb-4">Lista de reservas</h5>
 
                 <div class="table-responsive">
-                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#Modal_hoteles">
-                        Nuevo Hotel
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#Modal_reservas">
+                        Nueva reserva
                     </button>
                     <table class="table text-nowrap mb-0 align-middle">
                         <thead class="text-dark fs-4">
@@ -31,7 +31,7 @@
                                 </th>
                             </tr>
                         </thead>
-                        <tbody id="tabla_hoteles">
+                        <tbody id="tabla_reservas">
 
                         </tbody>
                     </table>
@@ -47,12 +47,12 @@
 
 
 <!-- Modal -->
-<div class="modal fade" id="Modal_hoteles" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+<div class="modal fade" id="Modal_reservas" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
-            <form method="post" id="frm_hoteles">
+            <form method="post" id="frm_reservas">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="staticBackdropLabel">Hoteles</h5>
+                    <h5 class="modal-title" id="staticBackdropLabel">Reserva</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
@@ -87,4 +87,4 @@
 
 <?php require_once('../html/script2.php') ?>
 
-<script src="hoteles.js"></script>
+<script src="reservas.js"></script>
